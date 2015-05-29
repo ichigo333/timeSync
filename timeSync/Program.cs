fa4b3b05fafbda9d43cb30b5fdf7450ec12eec47
Alex <alex333sh@gmail.com>
2015-05-29 01:42:54 -0400

    main clean up didn't get commited

--- timeSync/Program.cs
+++ timeSync/Program.cs
@@ -1,21 +1,12 @@
 ﻿using System;
-using System.Runtime.Serialization.Json;
 using System.IO;
 using System.Collections.Generic;
-using System.Xml;
-//using System.Xml.Serialization;
-using System.Runtime.Serialization;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-
-
 
 namespace timeSync {
   class MainClass {
     public static void Main(string[] args) {
-      Console.WriteLine("Hello World!");
 
-      var datacontractserializer = new DataContractJsonSerializer(typeof(ListOfNotes));
       var stream = new StreamReader("E:\\test.json");
       var json = stream.ReadToEnd().ToString();
 
@@ -23,30 +14,9 @@ namespace timeSync {
       foreach (var token in tokens) {
         Console.WriteLine(token.SelectToken("id").ToString() + " : " + (DateTime)token.SelectToken("target_date") + " : " + token.SelectToken("label"));
       }
-
-     
-      //var test = (Note)datacontractserializer.ReadObject(fileStream);
-	
+        	
       Console.ReadLine();
 
-    }
-
-    [DataContract]
-    public class ListOfNotes {
-      [DataMember]
-      private List<Note> notes = new List<Note>();
-
-      public void addNote(Note note) {
-        notes.Add(note);
-      }
-    }
-
-    [DataContract]
-    public class Note {
-      [DataMember]
-      public string type;
-      [DataMember]
-      public string id;
     }
   }
 }
