4c8f228510c57189e9454f98e390f4fe841bab1e
Alex <alex333sh@gmail.com>
2015-07-29 00:29:55 -0400

    experimenting with note object

--- timeSync/Note.cs
+++ timeSync/Note.cs
@@ -3,8 +3,13 @@
 namespace timeSync {
   public class Note {
     public string Id { get; set; }
-    public string Type { get; set; }
+    public string Label { get; set; }
     public DateTime TargetDate { get; set; }
+
+    public override string ToString() {
+      return string.Format("{0} : {1} : {2}", Id, TargetDate, Label);
+    }
+
   }
 }
 
--- timeSync/Program.cs
+++ timeSync/Program.cs
@@ -7,14 +7,23 @@ namespace timeSync {
   class MainClass {
     public static void Main(string[] args) {
 
-      var stream = new StreamReader("E:\\test.json");
+      var stream = new StreamReader("E:\\test1.json");
       var json = stream.ReadToEnd().ToString();
+      var counter = 0;
 
       JArray tokens = JArray.Parse(json);
       foreach (var token in tokens) {
-        Console.WriteLine(token.SelectToken("id").ToString() + " : " + (DateTime)token.SelectToken("target_date") + " : " + token.SelectToken("label"));
+        var note = new Note();
+        note.Id = token.SelectToken("id").ToString();
+        note.TargetDate = (DateTime)token.SelectToken("target_date");
+        note.Label = token.SelectToken("label").ToString();
+
+        counter++;
+
+        Console.WriteLine(note.ToString());
       }
-        	
+
+      Console.WriteLine("processed: " + counter + " records");
       Console.ReadLine();
 
     }
